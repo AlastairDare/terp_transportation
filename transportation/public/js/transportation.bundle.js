bef069922ecc66cb2e758cab849774d486a6ce4d
AlastairDare <dare.alastair@gmail.com>
2024-11-02 13:36:43 +0200

    added date validations with checkboxes to trailer and vehicle

--- transportation/public/js/transportation.bundle.js
+++ transportation/public/js/transportation.bundle.js
@@ -1,2 +1,3 @@
 import './custom_icons';
 import '../../transportation/doctype/vehicle/vehicle.js';
+import '../../transportation/doctype/trailer/trailer.js';
--- transportation/transportation/doctype/trailer/trailer.js
+++ transportation/transportation/doctype/trailer/trailer.js
@@ -1,53 +1,10 @@
-frappe.ui.form.on('Vehicle', {
+frappe.ui.form.on('Trailer', {
     refresh: function(frm) {
-        console.log('Vehicle form refreshed');
         // Set up initial state for all dependent fields
         updateFieldVisibility(frm, 'certificate_of_roadworthiness', 'certificate_of_roadworthiness_expiration');
         updateFieldVisibility(frm, 'cross_border_road_transport_permit', 'cross_border_road_transport_permit_expiration');
         updateFieldVisibility(frm, 'warranty', 'warranty_expiration');
         updateLicensePlateFields(frm);
-    },
-    
-    setup: function(frm) {
-        console.log('Vehicle form setup');
-        frm.set_query('primary_trailer', function() {
-            return {
-                filters: {
-                    'status': 'Active'
-                }
-            };
-        });
-    },
-    
-    primary_trailer: function(frm) {
-        console.log('Primary trailer changed:', frm.doc.primary_trailer);
-        
-        if (!frm.doc.primary_trailer) {
-            console.log('Clearing secondary trailer');
-            frm.set_value('secondary_trailer', '');
-            frm.refresh_field('secondary_trailer');
-            return;
-        }
-        
-        console.log('Fetching trailer details');
-        frappe.call({
-            method: 'frappe.client.get',
-            args: {
-                doctype: 'Trailer',
-                name: frm.doc.primary_trailer
-            },
-            callback: function(response) {
-                console.log('Trailer response:', response);
-                if (response.message && response.message.paired_trailer) {
-                    console.log('Setting secondary trailer to:', response.message.paired_trailer);
-                    frm.set_value('secondary_trailer', response.message.paired_trailer);
-                } else {
-                    console.log('No paired trailer found');
-                    frm.set_value('secondary_trailer', '');
-                }
-                frm.refresh_field('secondary_trailer');
-            }
-        });
     },
 
     // Handle Certificate of Roadworthiness changes
