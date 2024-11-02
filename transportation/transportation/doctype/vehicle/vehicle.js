45285237c5d6c4068bc10dff32bea601d572b21a
AlastairDare <dare.alastair@gmail.com>
2024-11-02 10:20:26 +0200

    debug logs for truck trailer pairing

--- transportation/transportation/doctype/vehicle/vehicle.js
+++ transportation/transportation/doctype/vehicle/vehicle.js
@@ -1,5 +1,10 @@
 frappe.ui.form.on('Vehicle', {
+    refresh: function(frm) {
+        console.log('Vehicle form refreshed');
+    },
+    
     setup: function(frm) {
+        console.log('Vehicle form setup');
         frm.set_query('primary_trailer', function() {
             return {
                 filters: {
@@ -10,12 +15,16 @@ frappe.ui.form.on('Vehicle', {
     },
     
     primary_trailer: function(frm) {
+        console.log('Primary trailer changed:', frm.doc.primary_trailer);
+        
         if (!frm.doc.primary_trailer) {
+            console.log('Clearing secondary trailer');
             frm.set_value('secondary_trailer', '');
             frm.refresh_field('secondary_trailer');
             return;
         }
         
+        console.log('Fetching trailer details');
         frappe.call({
             method: 'frappe.client.get',
             args: {
@@ -23,9 +32,12 @@ frappe.ui.form.on('Vehicle', {
                 name: frm.doc.primary_trailer
             },
             callback: function(response) {
+                console.log('Trailer response:', response);
                 if (response.message && response.message.paired_trailer) {
+                    console.log('Setting secondary trailer to:', response.message.paired_trailer);
                     frm.set_value('secondary_trailer', response.message.paired_trailer);
                 } else {
+                    console.log('No paired trailer found');
                     frm.set_value('secondary_trailer', '');
                 }
                 frm.refresh_field('secondary_trailer');
--- transportation/transportation/doctype/vehicle/vehicle.py
+++ transportation/transportation/doctype/vehicle/vehicle.py
@@ -2,9 +2,15 @@ import frappe
 from frappe import _
 
 def validate(doc, method):
+    frappe.logger().debug(f"Validating vehicle {doc.name}")
+    
     if doc.primary_trailer:
+        frappe.logger().debug(f"Primary trailer selected: {doc.primary_trailer}")
+        
         # Check if trailer exists and is active
         trailer = frappe.get_doc("Trailer", doc.primary_trailer)
+        frappe.logger().debug(f"Trailer status: {trailer.status}")
+        
         if trailer.status != "Active":
             frappe.throw(_("Trailer {0} is not active. Only active trailers can be assigned to vehicles.").format(
                 trailer.name))
@@ -15,17 +21,21 @@ def validate(doc, method):
             ["name", "truck_number"], as_dict=1)
             
         if assigned_vehicle:
+            frappe.logger().debug(f"Trailer already assigned to: {assigned_vehicle}")
             frappe.throw(_("Trailer {0} is currently assigned to vehicle {1} ({2}). Please unassign it first.").format(
                 trailer.name, assigned_vehicle.name, assigned_vehicle.truck_number))
         
         # Auto-populate secondary trailer if primary trailer has a pair
         if trailer.paired_trailer:
+            frappe.logger().debug(f"Found paired trailer: {trailer.paired_trailer}")
             paired_trailer = frappe.get_doc("Trailer", trailer.paired_trailer)
             if paired_trailer.status != "Active":
                 frappe.throw(_("The paired trailer {0} is not active. Cannot assign trailer pair to vehicle.").format(
                     paired_trailer.name))
             doc.secondary_trailer = trailer.paired_trailer
         else:
+            frappe.logger().debug("No paired trailer found")
             doc.secondary_trailer = None
     else:
+        frappe.logger().debug("No primary trailer selected")
         doc.secondary_trailer = None
