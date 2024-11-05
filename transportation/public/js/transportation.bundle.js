9854e93eca4e8d3e21b7f9a6f711a4ec71245bc2
AlastairDare <dare.alastair@gmail.com>
2024-11-05 10:47:08 +0200

    something wrong with issues

--- transportation/public/js/transportation.bundle.js
+++ transportation/public/js/transportation.bundle.js
@@ -2,5 +2,4 @@ import './custom_icons';
 import '../../transportation/doctype/vehicle/vehicle.js';
 import '../../transportation/doctype/trailer/trailer.js';
 import '../../transportation/doctype/transportation_asset/transportation_asset.js';
-//import '../../transportation/doctype/issues/issues.js';
 import '../../transportation/doctype/asset_unified_maintenance/asset_unified_maintenance.js';
