fd0da470fd13bbf0df175ce7bbc0e118351ee972
AlastairDare <dare.alastair@gmail.com>
2024-11-05 11:51:01 +0200

    reworked unified maint

--- transportation/transportation/doctype/asset_unified_maintenance/asset_unified_maintenance.js
+++ transportation/transportation/doctype/asset_unified_maintenance/asset_unified_maintenance.js
@@ -35,105 +35,6 @@ frappe.ui.form.on('Asset Unified Maintenance', {
     }
 });
 
-frappe.ui.form.on('Stock Entry Detail', {
-    s_warehouse: function(frm, cdt, cdn) {
-        update_available_qty(frm, cdt, cdn);
-    },
-    
-    item_code: function(frm, cdt, cdn) {
-        let row = locals[cdt][cdn];
-        if (row.item_code && row.s_warehouse) {
-            // Get item details including UOM and conversion factors
-            frappe.call({
-                method: 'frappe.client.get',
-                args: {
-                    doctype: 'Item',
-                    name: row.item_code
-                },
-                callback: function(r) {
-                    if (r.message) {
-                        let item = r.message;
-                        frappe.model.set_value(cdt, cdn, 'item_name', item.item_name);
-                        frappe.model.set_value(cdt, cdn, 'uom', item.stock_uom);
-                        frappe.model.set_value(cdt, cdn, 'stock_uom', item.stock_uom);
-                        frappe.model.set_value(cdt, cdn, 'conversion_factor', 1);
-                        update_available_qty(frm, cdt, cdn);
-                    }
-                }
-            });
-        }
-    },
-    
-    uom: function(frm, cdt, cdn) {
-        let row = locals[cdt][cdn];
-        if (row.uom && row.item_code) {
-            get_uom_conversion_factor(frm, cdt, cdn);
-        }
-    },
-    
-    qty: function(frm, cdt, cdn) {
-        let row = locals[cdt][cdn];
-        calculate_amount(frm, row);
-        frm.refresh_field('stock_items');
-    },
-    
-    basic_rate: function(frm, cdt, cdn) {
-        let row = locals[cdt][cdn];
-        calculate_amount(frm, row);
-        frm.refresh_field('stock_items');
-    }
-});
-
-function get_uom_conversion_factor(frm, cdt, cdn) {
-    let row = locals[cdt][cdn];
-    frappe.call({
-        method: 'erpnext.stock.get_item_details.get_uom_conv_factor',
-        args: {
-            uom: row.uom,
-            stock_uom: row.stock_uom
-        },
-        callback: function(r) {
-            if (r.message) {
-                frappe.model.set_value(cdt, cdn, 'conversion_factor', r.message);
-                update_available_qty(frm, cdt, cdn);
-            }
-        }
-    });
-}
-
-function update_available_qty(frm, cdt, cdn) {
-    let row = locals[cdt][cdn];
-    if (row.item_code && row.s_warehouse) {
-        frappe.call({
-            method: 'erpnext.stock.utils.get_stock_balance',
-            args: {
-                item_code: row.item_code,
-                warehouse: row.s_warehouse,
-                posting_date: frappe.datetime.get_today(),
-                posting_time: frappe.datetime.now_time()
-            },
-            callback: function(r) {
-                if (r.message) {
-                    let stock_qty = r.message[0];
-                    let available_qty = stock_qty;
-                    if (row.conversion_factor) {
-                        available_qty = stock_qty / row.conversion_factor;
-                    }
-                    frappe.model.set_value(cdt, cdn, 'available_qty', available_qty);
-                } else {
-                    frappe.model.set_value(cdt, cdn, 'available_qty', 0);
-                }
-            }
-        });
-    }
-}
-
-function calculate_amount(frm, row) {
-    if (!row) return;
-    row.amount = (row.qty || 0) * (row.basic_rate || 0);
-    row.basic_amount = row.amount;
-}
-
 function update_field_labels(frm) {
     const type = frm.doc.maintenance_type;
     const label_prefix = type || 'Maintenance';
@@ -144,4 +45,52 @@ function update_field_labels(frm) {
         `${label_prefix} Begin Date`);
     frm.set_df_property('complete_date', 'label',
         `${label_prefix} Complete Date`);
+}
+
+frappe.ui.form.on('Asset Maintenance Stock Item', {
+    item_code: function(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.item_code && row.warehouse) {
+            get_item_rate(frm, row);
+        }
+    },
+    
+    warehouse: function(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.item_code && row.warehouse) {
+            get_item_rate(frm, row);
+        }
+    },
+    
+    qty: function(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        calculate_amount(row);
+        frm.refresh_field('stock_items');
+    }
+});
+
+function get_item_rate(frm, row) {
+    frappe.call({
+        method: 'frappe.client.get_value',
+        args: {
+            doctype: 'Bin',
+            filters: {
+                'item_code': row.item_code,
+                'warehouse': row.warehouse
+            },
+            fieldname: ['valuation_rate']
+        },
+        callback: function(r) {
+            if (r.message && r.message.valuation_rate) {
+                frappe.model.set_value(row.doctype, row.name, 'rate', r.message.valuation_rate);
+                calculate_amount(row);
+                frm.refresh_field('stock_items');
+            }
+        }
+    });
+}
+
+function calculate_amount(row) {
+    let amount = flt(row.qty) * flt(row.rate);
+    frappe.model.set_value(row.doctype, row.name, 'amount', amount);
 }
--- transportation/transportation/doctype/asset_unified_maintenance/asset_unified_maintenance.py
+++ transportation/transportation/doctype/asset_unified_maintenance/asset_unified_maintenance.py
@@ -1,6 +1,6 @@
 import frappe
 from frappe import _
-from frappe.utils import getdate
+from frappe.utils import getdate, flt
 from frappe.model.document import Document
 
 class AssetUnifiedMaintenance(Document):
@@ -8,19 +8,19 @@ class AssetUnifiedMaintenance(Document):
         self.validate_dates()
         self.validate_stock_quantities()
         self.calculate_total_cost()
-        
+    
     def validate_dates(self):
         if self.complete_date and getdate(self.complete_date) < getdate(self.begin_date):
             frappe.throw(_("Complete Date cannot be before Begin Date"))
-            
+    
     def validate_stock_quantities(self):
         if self.execution_type == "Internal" and self.stock_items:
             for item in self.stock_items:
-                actual_qty = self.get_stock_qty(item.item_code, item.s_warehouse)
-                if item.qty > actual_qty:
+                actual_qty = self.get_stock_qty(item.item_code, item.warehouse)
+                if flt(item.qty) > flt(actual_qty):
                     frappe.throw(
                         _("Row {0}: Quantity ({1}) cannot be greater than available quantity ({2}) for item {3} in warehouse {4}")
-                        .format(item.idx, item.qty, actual_qty, item.item_code, item.s_warehouse)
+                        .format(item.idx, item.qty, actual_qty, item.item_code, item.warehouse)
                     )
     
     def get_stock_qty(self, item_code, warehouse):
@@ -29,20 +29,63 @@ class AssetUnifiedMaintenance(Document):
             {"item_code": item_code, "warehouse": warehouse},
             "actual_qty"
         )
-        return bin_qty or 0
+        return flt(bin_qty) or 0
+    
+    def get_valuation_rate(self, item_code, warehouse):
+        """Get the correct valuation rate based on FIFO/LIFO"""
+        # Get the valuation method for the item
+        valuation_method = frappe.db.get_value("Item", item_code, "valuation_method")
+        
+        # Get stock ledger entries for this item in this warehouse
+        sle = frappe.get_list(
+            "Stock Ledger Entry",
+            filters={
+                "item_code": item_code,
+                "warehouse": warehouse,
+                "is_cancelled": 0
+            },
+            fields=["valuation_rate", "actual_qty", "posting_date"],
+            order_by="posting_date DESC, creation DESC"
+        )
+        
+        if not sle:
+            return 0
+        
+        if valuation_method == "FIFO":
+            # For FIFO, get the oldest available stock's rate
+            for entry in reversed(sle):
+                if flt(entry.actual_qty) > 0:
+                    return flt(entry.valuation_rate)
+        else:  # LIFO
+            # For LIFO, get the newest available stock's rate
+            for entry in sle:
+                if flt(entry.actual_qty) > 0:
+                    return flt(entry.valuation_rate)
+        
+        # If no positive stock found, return the latest valuation rate
+        return flt(sle[0].valuation_rate) if sle else 0
+    
+    def update_stock_rates(self):
+        """Update rates for all stock items based on current valuation"""
+        if self.execution_type == "Internal" and self.stock_items:
+            for item in self.stock_items:
+                rate = self.get_valuation_rate(item.item_code, item.warehouse)
+                item.rate = rate
+                item.amount = flt(item.qty) * flt(rate)
     
     def calculate_total_cost(self):
         if self.execution_type == "Internal":
             total = 0
             if self.stock_items:
+                self.update_stock_rates()  # Update rates before calculating total
                 for item in self.stock_items:
-                    total += item.amount
+                    total += flt(item.amount)
             self.total_cost = total
         else:
             if self.purchase_invoice:
                 pi = frappe.get_doc("Purchase Invoice", self.purchase_invoice)
                 self.total_cost = pi.total
-
+    
     def get_last_maintenance_dates(self):
         last_dates = {
             "last_service_date": None,
@@ -87,13 +130,55 @@ class AssetUnifiedMaintenance(Document):
             last_dates["last_repair_date"] = last_repair[0].complete_date
             
         return last_dates
-        
+    
+    def before_save(self):
+        """Update rates and amounts before saving"""
+        self.update_stock_rates()
+    
     def on_submit(self):
         if self.execution_type == "Internal" and self.stock_items:
             self.create_stock_entry()
-            
+    
     def create_stock_entry(self):
+        """Create a stock entry for consumed items"""
         if not self.stock_items:
             return
             
-        stock_entry = frappe.new
+        stock_entry = frappe.new_doc("Stock Entry")
+        stock_entry.stock_entry_type = "Material Issue"
+        stock_entry.purpose = "Material Issue"
+        stock_entry.company = frappe.defaults.get_user_default("Company")
+        stock_entry.asset_maintenance = self.name
+        
+        for item in self.stock_items:
+            stock_entry.append("items", {
+                "s_warehouse": item.warehouse,
+                "item_code": item.item_code,
+                "qty": item.qty,
+                "basic_rate": item.rate,
+                "basic_amount": item.amount
+            })
+        
+        stock_entry.insert()
+        stock_entry.submit()
+        
+        frappe.msgprint(_("Stock Entry {0} created").format(
+            frappe.get_desk_link("Stock Entry", stock_entry.name)
+        ))
+    
+    def on_cancel(self):
+        """Cancel linked stock entry if exists"""
+        stock_entries = frappe.get_all(
+            "Stock Entry",
+            filters={
+                "asset_maintenance": self.name,
+                "docstatus": 1
+            }
+        )
+        
+        for se in stock_entries:
+            stock_entry = frappe.get_doc("Stock Entry", se.name)
+            stock_entry.cancel()
+            frappe.msgprint(_("Stock Entry {0} cancelled").format(
+                frappe.get_desk_link("Stock Entry", stock_entry.name)
+            ))
