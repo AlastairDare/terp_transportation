69be079f9335134c483a7ee21d86e8d6e45dc9e8
AlastairDare <dare.alastair@gmail.com>
2024-11-06 10:55:26 +0200

    fix py in wrong file

--- transportation/transportation/doctype/asset_unified_maintenance/asset_unified_maintenance.py
+++ transportation/transportation/doctype/asset_unified_maintenance/asset_unified_maintenance.py
@@ -1,154 +1,106 @@
-frappe.ui.form.on('Asset Unified Maintenance', {
-    refresh: function(frm) {
-        update_field_labels(frm);
-        
-        // Set default company if not set
-        if (!frm.doc.company) {
-            frm.set_value('company', frappe.defaults.get_user_default('Company'));
-        }
-        
-        // Always update warranty display
-        update_warranty_display(frm);
-        
-        if(frm.doc.docstatus === 1) {
-            frm.add_custom_button(__('Stock Entry'), function() {
-                frappe.route_options = {
-                    'reference_doctype': frm.doc.doctype,
-                    'reference_name': frm.doc.name
-                };
-                frappe.set_route('List', 'Stock Entry');
-            }, __("View"));
-        }
-    },
-    
-    onload: function(frm) {
-        // Set default company if not set
-        if (!frm.doc.company) {
-            frm.set_value('company', frappe.defaults.get_user_default('Company'));
-        }
-        update_warranty_display(frm);
-    },
-    
-    asset: function(frm) {
-        if (frm.doc.asset) {
-            frappe.call({
-                method: 'get_last_maintenance_dates',
-                doc: frm.doc,
-                callback: function(r) {
-                    if (r.message) {
-                        frm.set_value('last_service_date', r.message.last_service_date);
-                        frm.set_value('last_repair_date', r.message.last_repair_date);
-                        frm.refresh_fields(['last_service_date', 'last_repair_date']);
-                    }
-                }
-            });
-            update_warranty_display(frm);
-        }
-    },
-    
-    warranty_status: function(frm) {
-        update_warranty_display(frm);
-    },
-    
-    maintenance_type: function(frm) {
-        update_field_labels(frm);
-    },
-    
-    execution_type: function(frm) {
-        if (frm.doc.execution_type === 'Internal') {
-            frm.set_value('vendor', '');
-            frm.set_value('purchase_invoice', '');
-        } else {
-            frm.clear_table('items');
-            frm.set_value('assigned_employee', '');
-        }
-        frm.refresh_fields();
-    }
-});
+import frappe
+from frappe import _
+from frappe.utils import getdate, flt, cstr
+from frappe.model.document import Document
 
-// Function to update warranty display
-function update_warranty_display(frm) {
-    // Always show warranty status text
-    let warranty_html = '';
-    if (frm.doc.warranty_status) {
-        warranty_html = '<div class="alert alert-success">Asset is in Warranty</div>';
-    } else {
-        warranty_html = '<div class="alert alert-warning">Asset out of Warranty</div>';
-    }
+class AssetUnifiedMaintenance(Document):
+    def validate(self):
+        self.validate_dates()
+        if not self.company:
+            self.company = frappe.defaults.get_user_default("Company")
+        
+    def on_update(self):
+        if self.maintenance_status == "Complete" and hasattr(self, 'get_doc_before_save'):
+            previous = self.get_doc_before_save()
+            if previous and previous.maintenance_status != "Complete":
+                self.create_stock_entry()
     
-    // Remove existing display if any
-    $('.warranty-status-display').remove();
-    
-    // Add new display after the warranty_status field
-    $(frm.fields_dict.warranty_status.wrapper)
-        .append('<div class="warranty-status-display">' + warranty_html + '</div>');
-    
-    // Hide the checkbox
-    frm.set_df_property('warranty_status', 'hidden', 1);
-}
+    def validate_dates(self):
+        if self.complete_date and getdate(self.complete_date) < getdate(self.begin_date):
+            frappe.throw(_("Complete Date cannot be before Begin Date"))
 
-// Add handlers for the items table
-frappe.ui.form.on('Stock Entry Detail', {
-    items_add: function(frm, cdt, cdn) {
-        let row = locals[cdt][cdn];
-        if (!row.s_warehouse) {
-            row.s_warehouse = frappe.defaults.get_default('stock_warehouse');
-        }
-        frm.refresh_field('items');
-    },
-    
-    item_code: function(frm, cdt, cdn) {
-        let row = locals[cdt][cdn];
-        if (!frm.doc.company) {
-            frm.set_value('company', frappe.defaults.get_user_default('Company'));
-            frappe.throw(__('Please set the Company first'));
-            return;
+    @frappe.whitelist()
+    def get_last_maintenance_dates(self):
+        last_dates = {
+            "last_service_date": None,
+            "last_repair_date": None
         }
         
-        if (row.item_code) {
-            frappe.call({
-                method: 'erpnext.stock.get_item_details.get_item_details',
-                type: "POST",
-                args: {
-                    args: {
-                        item_code: row.item_code,
-                        company: frm.doc.company,
-                        warehouse: row.s_warehouse || frappe.defaults.get_default('stock_warehouse'),
-                        doctype: frm.doctype,
-                        buying_price_list: frappe.defaults.get_default('buying_price_list'),
-                        currency: frappe.defaults.get_default('Currency'),
-                        name: frm.doc.name,
-                        qty: row.qty || 1,
-                        child_docname: row.name
-                    }
-                },
-                callback: function(r) {
-                    if(r.message) {
-                        $.each(r.message, function(k, v) {
-                            if(!row[k]) row[k] = v;
-                        });
-                        frm.refresh_field('items');
-                    }
-                }
-            });
-        }
-    },
+        if not self.asset:
+            return last_dates
+        
+        # Get last service date
+        last_service = frappe.get_list(
+            "Asset Unified Maintenance",
+            filters={
+                "asset": self.asset,
+                "maintenance_type": "Service",
+                "maintenance_status": "Complete",
+                "name": ["!=", self.name]
+            },
+            fields=["complete_date"],
+            order_by="complete_date desc",
+            limit=1
+        )
+        
+        if last_service:
+            last_dates["last_service_date"] = last_service[0].complete_date
+        
+        # Get last repair date
+        last_repair = frappe.get_list(
+            "Asset Unified Maintenance",
+            filters={
+                "asset": self.asset,
+                "maintenance_type": "Repair",
+                "maintenance_status": "Complete",
+                "name": ["!=", self.name]
+            },
+            fields=["complete_date"],
+            order_by="complete_date desc",
+            limit=1
+        )
+        
+        if last_repair:
+            last_dates["last_repair_date"] = last_repair[0].complete_date
+        
+        return last_dates
     
-    qty: function(frm, cdt, cdn) {
-        let row = locals[cdt][cdn];
-        frappe.model.set_value(cdt, cdn, 'transfer_qty', 
-            flt(row.qty) * flt(row.conversion_factor));
-    }
-});
-
-function update_field_labels(frm) {
-    const type = frm.doc.maintenance_type;
-    const label_prefix = type || 'Maintenance';
-    
-    frm.set_df_property('maintenance_status', 'label',
-        `${label_prefix} Status`);
-    frm.set_df_property('begin_date', 'label',
-        `${label_prefix} Begin Date`);
-    frm.set_df_property('complete_date', 'label',
-        `${label_prefix} Complete Date`);
-}
+    def create_stock_entry(self):
+        if not self.items:
+            return
+            
+        stock_entry = frappe.new_doc("Stock Entry")
+        stock_entry.stock_entry_type = "Material Issue"
+        stock_entry.purpose = "Material Issue"
+        stock_entry.company = self.company
+        stock_entry.reference_doctype = self.doctype
+        stock_entry.reference_name = self.name
+        
+        default_warehouse = frappe.db.get_single_value("Stock Settings", "default_warehouse")
+        
+        for item in self.items:
+            if not item.s_warehouse:
+                item.s_warehouse = default_warehouse
+                
+            stock_entry.append("items", {
+                "item_code": item.item_code,
+                "qty": item.qty,
+                "uom": item.uom,
+                "stock_uom": item.stock_uom,
+                "conversion_factor": item.conversion_factor,
+                "s_warehouse": item.s_warehouse,
+                "basic_rate": item.basic_rate,
+                "basic_amount": item.basic_amount,
+                "amount": item.amount,
+                "cost_center": item.cost_center or frappe.db.get_value("Company", self.company, "cost_center")
+            })
+        
+        try:
+            stock_entry.insert()
+            stock_entry.submit()
+            frappe.msgprint(_("Stock Entry {0} created and submitted").format(
+                frappe.get_desk_link("Stock Entry", stock_entry.name)
+            ))
+        except Exception as e:
+            frappe.msgprint(_("Error creating Stock Entry: {0}").format(str(e)))
+            raise e
