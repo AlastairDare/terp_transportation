cafc7fe2ef1d05425e934af46a48ccea383a015b
AlastairDare <dare.alastair@gmail.com>
2024-11-06 10:09:21 +0200

    trying stock detail

--- transportation/transportation/doctype/asset_unified_maintenance/asset_unified_maintenance.js
+++ transportation/transportation/doctype/asset_unified_maintenance/asset_unified_maintenance.js
@@ -1,6 +1,16 @@
 frappe.ui.form.on('Asset Unified Maintenance', {
     refresh: function(frm) {
         update_field_labels(frm);
+        
+        if(frm.doc.docstatus === 1) {
+            frm.add_custom_button(__('Stock Entry'), function() {
+                frappe.route_options = {
+                    'reference_doctype': frm.doc.doctype,
+                    'reference_name': frm.doc.name
+                };
+                frappe.set_route('List', 'Stock Entry');
+            }, __("View"));
+        }
     },
     
     asset: function(frm) {
@@ -28,62 +38,103 @@ frappe.ui.form.on('Asset Unified Maintenance', {
             frm.set_value('vendor', '');
             frm.set_value('purchase_invoice', '');
         } else {
-            frm.clear_table('stock_items');
+            frm.clear_table('items');
             frm.set_value('assigned_employee', '');
         }
         frm.refresh_fields();
+    },
+
+    calculate_basic_amount: function(frm, item) {
+        item.basic_amount = flt(flt(item.transfer_qty) * flt(item.basic_rate),
+            precision("basic_amount", item));
+
+        frm.events.calculate_amount(frm, item);
+    },
+
+    calculate_amount: function(frm, item) {
+        item.amount = flt(item.basic_amount);
+        frm.refresh_field('items');
     }
 });
 
-// Handle Stock Items child table
-frappe.ui.form.on('Asset Maintenance Stock Item', {
-    stock_items_add: function(frm, cdt, cdn) {
+// Add handlers for the items table
+frappe.ui.form.on('Stock Entry Detail', {
+    items_add: function(frm, cdt, cdn) {
         let row = locals[cdt][cdn];
-        frappe.model.set_value(cdt, cdn, 'warehouse', frm.doc.source_warehouse);
+        if (!row.s_warehouse) {
+            row.s_warehouse = frappe.defaults.get_default('stock_warehouse');
+        }
+        frm.refresh_field('items');
     },
     
     item_code: function(frm, cdt, cdn) {
         let row = locals[cdt][cdn];
-        if (row.item_code && row.warehouse) {
-            get_item_details(frm, row);
-        }
-    },
-    
-    warehouse: function(frm, cdt, cdn) {
-        let row = locals[cdt][cdn];
-        if (row.item_code && row.warehouse) {
-            get_item_details(frm, row);
+        if (row.item_code) {
+            frappe.call({
+                method: 'erpnext.stock.get_item_details.get_item_details',
+                args: {
+                    item_code: row.item_code,
+                    company: frm.doc.company,
+                    warehouse: row.s_warehouse,
+                    doctype: frm.doctype,
+                    buying_price_list: frappe.defaults.get_default('buying_price_list'),
+                    currency: frappe.defaults.get_default('Currency'),
+                    name: frm.doc.name,
+                    qty: row.qty || 1,
+                    child_docname: row.name
+                },
+                callback: function(r) {
+                    if(r.message) {
+                        $.each(r.message, function(k, v) {
+                            if(!row[k]) row[k] = v;
+                        });
+                        frm.refresh_field('items');
+                        frm.events.set_basic_rate(frm, cdt, cdn);
+                    }
+                }
+            });
         }
     },
     
     qty: function(frm, cdt, cdn) {
         let row = locals[cdt][cdn];
-        calculate_amount(frm, row);
+        frappe.model.set_value(cdt, cdn, 'transfer_qty', 
+            flt(row.qty) * flt(row.conversion_factor));
+        frm.events.set_basic_rate(frm, cdt, cdn);
+    },
+
+    set_basic_rate: function(frm, cdt, cdn) {
+        const item = locals[cdt][cdn];
+        item.transfer_qty = flt(item.qty) * flt(item.conversion_factor);
+
+        const args = {
+            item_code: item.item_code,
+            posting_date: frappe.datetime.nowdate(),
+            posting_time: frappe.datetime.now_time(),
+            warehouse: cstr(item.s_warehouse) || cstr(item.t_warehouse),
+            serial_no: item.serial_no,
+            batch_no: item.batch_no,
+            company: frm.doc.company,
+            qty: item.s_warehouse ? -1 * flt(item.transfer_qty) : flt(item.transfer_qty),
+            voucher_type: frm.doc.doctype,
+            voucher_no: item.name,
+            allow_zero_valuation: 1,
+        };
+
+        if (item.item_code || item.serial_no) {
+            frappe.call({
+                method: "erpnext.stock.utils.get_incoming_rate",
+                args: {
+                    args: args
+                },
+                callback: function(r) {
+                    frappe.model.set_value(cdt, cdn, 'basic_rate', r.message || 0.0);
+                    frm.events.calculate_basic_amount(frm, item);
+                }
+            });
+        }
     }
 });
-
-function get_item_details(frm, row) {
-    frappe.call({
-        method: 'get_valuation_rate',
-        doc: frm.doc,
-        args: {
-            'item_code': row.item_code,
-            'warehouse': row.warehouse
-        },
-        callback: function(r) {
-            if (r.message) {
-                frappe.model.set_value(row.doctype, row.name, 'rate', r.message);
-                calculate_amount(frm, row);
-            }
-        }
-    });
-}
-
-function calculate_amount(frm, row) {
-    let amount = flt(row.qty) * flt(row.rate);
-    frappe.model.set_value(row.doctype, row.name, 'amount', amount);
-    frm.refresh_field('stock_items');
-}
 
 function update_field_labels(frm) {
     const type = frm.doc.maintenance_type;
