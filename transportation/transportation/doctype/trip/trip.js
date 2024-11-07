1cf389ecb572a9bed284c167d92d4041cfb43ae5
AlastairDare <dare.alastair@gmail.com>
2024-11-07 15:29:10 +0200

    modified js not to fail on subsequent trips and odo start populate

--- transportation/transportation/doctype/trip/trip.js
+++ transportation/transportation/doctype/trip/trip.js
@@ -1,86 +1,129 @@
 frappe.ui.form.on('Trip', {
     refresh: function(frm) {
-        // Add any refresh logic here if needed
+        // Add custom buttons or refresh logic if needed
     },
 
-    validate: function(frm) {
-        // Additional validation if needed
+    onload: function(frm) {
+        // Set custom queries for truck and trailer fields
+        frm.set_query('truck', function() {
+            return {
+                filters: {
+                    'transportation_asset_type': 'Truck'
+                }
+            };
+        });
+
+        frm.set_query('trailer_1', function() {
+            return {
+                filters: {
+                    'transportation_asset_type': 'Trailer'
+                }
+            };
+        });
+
+        frm.set_query('trailer_2', function() {
+            return {
+                filters: {
+                    'transportation_asset_type': 'Trailer'
+                }
+            };
+        });
     },
 
     truck: function(frm) {
-        // Clear trailer fields if truck is cleared
+        // Clear related fields if truck is cleared
         if (!frm.doc.truck) {
             frm.set_value('trailer_1', '');
             frm.set_value('trailer_2', '');
+            frm.set_value('odo_start', '');
+            return;
         }
         
-        // Set initial odometer if empty
-        if (!frm.doc.odo_start) {
-            frm.set_initial_odometer();
-        }
+        // Get the last odometer reading
+        frappe.call({
+            method: 'transportation.doctype.trip.trip.get_last_odometer_reading',
+            args: {
+                'truck': frm.doc.truck,
+                'current_doc': frm.doc.name || null
+            },
+            callback: function(r) {
+                if (r.message && r.message.odo_end) {
+                    frm.set_value('odo_start', r.message.odo_end);
+                    frappe.show_alert({
+                        message: __('Odometer start set to {0} from trip {1}', 
+                            [r.message.odo_end, r.message.trip_name]),
+                        indicator: 'green'
+                    });
+                } else {
+                    frm.set_value('odo_start', 0);
+                    frappe.show_alert({
+                        message: __('No previous trip found for this truck. Starting from 0.'),
+                        indicator: 'blue'
+                    });
+                }
+            }
+        });
     },
 
     status: function(frm) {
-        // If status is being changed to Complete, set approver
+        // Handle status changes
         if (frm.doc.status === "Complete") {
-            // Get the previous version of the document
-            frappe.db.get_value(
-                'Trip',
-                frm.doc.name,
-                'status',
-                function(r) {
-                    if (r && r.status !== "Complete") {
-                        // Status is being changed to Complete, set approver
-                        frm.set_value('approver', frappe.session.user);
-                        
-                        // Show a notification
-                        frappe.show_alert({
-                            message: __('Trip marked as Complete by {0}', [frappe.session.user_fullname]),
-                            indicator: 'green'
-                        }, 5);
-                    }
-                }
-            );
+            let previous_status = frm.doc.__previous_status || frm.doc.status;
+            if (previous_status !== "Complete") {
+                frm.set_value('approver', frappe.session.user);
+                frappe.show_alert({
+                    message: __('Trip marked as Complete by {0}', [frappe.session.user_fullname]),
+                    indicator: 'green'
+                }, 5);
+            }
         }
     },
 
-    set_initial_odometer: function(frm) {
-        if (frm.doc.truck) {
-            frappe.call({
-                method: 'set_initial_odometer',
-                doc: frm.doc,
-                callback: function(r) {
-                    frm.refresh_field('odo_start');
-                }
-            });
-        }
+    odo_end: function(frm) {
+        calculateTotalDistance(frm);
+    },
+
+    odo_start: function(frm) {
+        calculateTotalDistance(frm);
+    },
+
+    second_mass: function(frm) {
+        calculateNetMass(frm);
+    },
+
+    first_mass: function(frm) {
+        calculateNetMass(frm);
     }
 });
 
-// Additional client script to enhance filtering
-frappe.ui.form.on('Trip', 'onload', function(frm) {
-    // Set custom queries for truck and trailer fields
-    frm.set_query('truck', function() {
-        return {
-            filters: {
-                'transportation_asset_type': 'Truck'
-            }
-        };
-    });
+// Helper functions
+function calculateTotalDistance(frm) {
+    if (frm.doc.odo_start && frm.doc.odo_end) {
+        let total = frm.doc.odo_end - frm.doc.odo_start;
+        if (total >= 0) {
+            frm.set_value('total_distance', total);
+        } else {
+            frappe.show_alert({
+                message: __('End odometer reading cannot be less than start reading'),
+                indicator: 'red'
+            });
+            frm.set_value('odo_end', '');
+            frm.set_value('total_distance', '');
+        }
+    }
+}
 
-    frm.set_query('trailer_1', function() {
-        return {
-            filters: {
-                'transportation_asset_type': 'Trailer'
-            }
-        };
-    });
-
-    frm.set_query('trailer_2', function() {
-        return {
-            filters: {
-                'transportation_asset_type': 'Trailer'
-            }
-        };
-    });
-});
+function calculateNetMass(frm) {
+    if (frm.doc.first_mass && frm.doc.second_mass) {
+        if (frm.doc.second_mass >= frm.doc.first_mass) {
+            frm.set_value('net_mass', frm.doc.second_mass - frm.doc.first_mass);
+        } else {
+            frappe.show_alert({
+                message: __('Second mass cannot be less than first mass'),
+                indicator: 'red'
+            });
+            frm.set_value('second_mass', '');
+            frm.set_value('net_mass', '');
+        }
+    }
+}
--- transportation/transportation/doctype/trip/trip.py
+++ transportation/transportation/doctype/trip/trip.py
@@ -2,23 +2,11 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 from frappe.model.document import Document
-from typing import Any
+from typing import Any, Dict, Optional
 
 class Trip(Document):
     def get_truck_query(self, doctype: str, txt: str, searchfield: str, start: int, page_len: int, filters: dict) -> list:
-        """Filter Transportation Assets to show only Trucks.
-        
-        Args:
-            doctype: The DocType being queried
-            txt: Search text entered by user
-            searchfield: Field being searched
-            start: Starting index for pagination
-            page_len: Number of results per page
-            filters: Additional filters
-            
-        Returns:
-            List of matching truck records
-        """
+        """Filter Transportation Assets to show only Trucks."""
         return frappe.db.sql("""
             SELECT name 
             FROM `tabTransportation Asset`
@@ -39,19 +27,7 @@ class Trip(Document):
         })
     
     def get_trailer_query(self, doctype: str, txt: str, searchfield: str, start: int, page_len: int, filters: dict) -> list:
-        """Filter Transportation Assets to show only Trailers.
-        
-        Args:
-            doctype: The DocType being queried
-            txt: Search text entered by user
-            searchfield: Field being searched
-            start: Starting index for pagination
-            page_len: Number of results per page
-            filters: Additional filters
-            
-        Returns:
-            List of matching trailer records
-        """
+        """Filter Transportation Assets to show only Trailers."""
         return frappe.db.sql("""
             SELECT name 
             FROM `tabTransportation Asset`
@@ -73,30 +49,69 @@ class Trip(Document):
     
     def validate(self):
         """Validate Trip document before saving."""
-        if not self.odo_start and self.truck:
-            self.set_initial_odometer()
-        
-        # Set approver when status changes to Complete
+        # Handle approver setting
         if self.has_value_changed('status') and self.status == "Complete":
             self.approver = frappe.session.user
+
+        # Validate odometer readings
+        if self.odo_start and self.odo_end:
+            if self.odo_end < self.odo_start:
+                frappe.throw(_("End odometer reading cannot be less than start reading"))
+            self.total_distance = self.odo_end - self.odo_start
+
+        # Validate date
+        if not self.date:
+            frappe.throw(_("Trip Date is required"))
+
+        # Validate mass readings if provided
+        if self.second_mass and self.first_mass:
+            if self.second_mass < self.first_mass:
+                frappe.throw(_("Second mass cannot be less than first mass"))
+            self.net_mass = self.second_mass - self.first_mass
+
+@frappe.whitelist()
+def get_last_odometer_reading(truck: str, current_doc: Optional[str] = None) -> Dict:
+    """Get the last odometer reading for a truck.
     
-    def set_initial_odometer(self):
-        """Set the initial odometer reading from the most recent trip for the same truck."""
-        last_trip = frappe.get_all(
-            "Trip",
-            filters={
-                "truck": self.truck,
-                "docstatus": 1,  # Only consider submitted documents
-                "name": ["!=", self.name]  # Exclude current document
-            },
-            fields=["odo_end"],
-            order_by="date desc, modified desc",
-            limit=1
-        )
+    Args:
+        truck: The truck ID to look up
+        current_doc: The current document name to exclude from the search
         
-        if last_trip:
-            self.odo_start = last_trip[0].odo_end
-            frappe.msgprint(
-                _("Odometer start value set to {} from the previous trip".format(self.odo_start)),
-                alert=True
-            )
+    Returns:
+        Dict containing the last odometer reading and trip information
+    """
+    if not truck:
+        return {
+            "odo_end": 0,
+            "trip_name": None,
+            "trip_date": None
+        }
+
+    filters = {
+        "truck": truck,
+        "docstatus": ["!=", 2],  # Include both draft (0) and submitted (1) documents
+    }
+    
+    if current_doc:
+        filters["name"] = ["!=", current_doc]
+    
+    last_trip = frappe.get_list(
+        "Trip",
+        filters=filters,
+        fields=["odo_end", "date", "name"],
+        order_by="date desc, creation desc",
+        limit=1
+    )
+    
+    if last_trip and last_trip[0].odo_end:
+        return {
+            "odo_end": last_trip[0].odo_end,
+            "trip_name": last_trip[0].name,
+            "trip_date": last_trip[0].date
+        }
+    
+    return {
+        "odo_end": 0,
+        "trip_name": None,
+        "trip_date": None
+    }
