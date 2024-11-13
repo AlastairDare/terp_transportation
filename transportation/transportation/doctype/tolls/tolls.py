36c19c00458afc4dbb4ba2df7ec4b17f1a182a8d
AlastairDare <dare.alastair@gmail.com>
2024-11-13 08:30:28 +0200

    tolls validate module level

--- transportation/transportation/doctype/tolls/tolls.py
+++ transportation/transportation/doctype/tolls/tolls.py
@@ -1,7 +1,19 @@
 import frappe
 from frappe.model.document import Document
 
+def validate(doc, method):
+    """Module-level validation for tolls"""
+    if not doc.transaction_date:
+        frappe.throw("Transaction Date & Time is required")
+        
+    if not doc.tolling_point:
+        frappe.throw("TA/Tolling Point is required")
+        
+    if not doc.etag_id:
+        frappe.throw("e-tag ID is required")
+        
+    if not doc.net_amount:
+        frappe.throw("Net Amount is required")
+
 class Tolls(Document):
-    def validate(self):
-        # Additional validation can be added here if needed
-        pass
+    pass  # All validation handled at module level
