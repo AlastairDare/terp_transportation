017481abc4181c2f1b40c431edfb2d70c6cd6303
AlastairDare <dare.alastair@gmail.com>
2024-11-20 16:39:55 +0200

    rename refuel to refuel capture and changed dashboard link

--- transportation/transportation/page/dashboard/dashboard.py
+++
@@ -1,8 +0,0 @@
-import frappe
-
-def get_context(context):
-    context.no_cache = 1
-    context.show_sidebar = True
-    context.title = "Dashboard"
-    # Add any initial dashboard configuration here
-    return context
