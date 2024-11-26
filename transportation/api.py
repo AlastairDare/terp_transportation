992bdc964f9b5e89726e8de4c9c17e2cde152e97
AlastairDare <dare.alastair@gmail.com>
2024-11-26 08:01:28 +0200

    redirect to operations

--- transportation/api.py
+++
@@ -1,6 +0,0 @@
-import frappe
-
-def set_home_page():
-    """Set the default home page for the user"""
-    if frappe.session.user:
-        frappe.db.set_value('User', frappe.session.user, 'home_page', '/app/operations')
--- transportation/public/js/transportation.bundle.js
+++ transportation/public/js/transportation.bundle.js
@@ -1,5 +1,4 @@
 import './custom_icons';
-import './redirect';
 import '../../transportation/doctype/vehicle/vehicle.js';
 import '../../transportation/doctype/trailer/trailer.js';
 import '../../transportation/doctype/transportation_asset/transportation_asset.js';
