2c906f743ea0c371e6dac2e8415ddf7158d8e25b
AlastairDare <dare.alastair@gmail.com>
2024-12-02 08:18:38 +0200

    trying to fix content

--- transportation/transportation/doctype/custom_workspace_config/custom_workspace_config.py
+++ transportation/transportation/doctype/custom_workspace_config/custom_workspace_config.py
@@ -18,56 +18,106 @@ class CustomWorkspaceConfig(Document):
     def generate_workspace_content(self):
         """Generate the workspace content in the format expected by ERPNext"""
         content = []
+        links = []
+        current_card = None
+        card_link_count = 0
         
         for item in sorted(self.workspace_content, key=lambda x: x.sequence):
             if item.item_type == "Header":
+                if current_card:
+                    # Update the link count for the previous card
+                    links[card_start_index]["link_count"] = card_link_count
+                
+                # Add header
                 content.append({
                     "type": "header",
                     "data": {
-                        "text": f"<span class='h4'><b>{item.header_text}</b></span>",
+                        "text": f"<span class=\\\"h4\\\"><b>{item.header_text}</b></span>",
                         "col": 12
                     }
                 })
-            else:  # Link
+                
+                # Add card
                 content.append({
-                    "type": "shortcut",
+                    "type": "card",
                     "data": {
-                        "name": item.link_label,
-                        "label": item.link_label,
-                        "link_to": item.link_to,
-                        "link_type": item.link_type,
-                        "type": "List" if item.link_type == "DocType" else "Link",
-                        "col": 4,
-                        "onboard": 0,
-                        "dependencies": [],
-                        "is_query_report": 0
+                        "card_name": item.header_text,
+                        "col": 4
                     }
                 })
+                
+                # Add card break to links
+                links.append({
+                    "hidden": 0,
+                    "is_query_report": 0,
+                    "label": item.header_text,
+                    "link_count": 0,  # Will be updated when we process next header
+                    "onboard": 0,
+                    "type": "Card Break"
+                })
+                card_start_index = len(links) - 1
+                current_card = item.header_text
+                card_link_count = 0
+                
+            else:  # Link
+                # Add link to content
+                content.append({
+                    "type": "link",
+                    "data": {
+                        "label": item.link_label,
+                        "link_type": item.link_type,
+                        "link_to": item.link_to,
+                        "type": "List" if item.link_type == "DocType" else "Link",
+                        "onboard": 1,
+                        "col": 4
+                    }
+                })
+                
+                # Add link to links array
+                links.append({
+                    "hidden": 0,
+                    "is_query_report": 0,
+                    "label": item.link_label,
+                    "link_count": 0,
+                    "link_to": item.link_to,
+                    "link_type": item.link_type,
+                    "onboard": 1,
+                    "type": "Link"
+                })
+                card_link_count += 1
         
-        return json.dumps(content)
+        # Update link count for the last card if exists
+        if current_card and len(links) > 0:
+            links[card_start_index]["link_count"] = card_link_count
+        
+        return json.dumps(content), links
     
     def after_insert(self):
+        """Create or update the ERPNext Workspace document"""
         workspace_name = f"custom-{self.workspace_name.lower().replace(' ', '-')}"
+        
+        content, links = self.generate_workspace_content()
         
         workspace_data = {
             "doctype": "Workspace",
-            "name": workspace_name,  # Add this line
+            "name": workspace_name,
             "icon": self.icon,
             "label": self.workspace_name,
             "module": "Transportation",
             "parent_page": "Transportation",
             "public": 1,
-            "content": self.generate_workspace_content(),
+            "content": content,
             "sequence_id": float(self.sequence),
             "title": self.workspace_name,
-            "is_hidden": not self.is_active,
-            "hide_custom": 1
+            "hide_custom": 1,
+            "links": links,
+            "is_hidden": not self.is_active
         }
         
         try:
             workspace = frappe.get_doc(workspace_data)
-            workspace.insert(ignore_permissions=True)  # Add ignore_permissions=True
-            frappe.db.commit()  # Add explicit commit
+            workspace.insert(ignore_permissions=True)
+            frappe.db.commit()
         except Exception as e:
             frappe.log_error(f"Error creating workspace: {str(e)}", "Workspace Creation Error")
             raise
@@ -99,10 +149,10 @@ class CustomWorkspaceConfig(Document):
         """Delete the associated workspace when this config is deleted"""
         workspace_name = f"custom-{self.workspace_name.lower().replace(' ', '-')}"
         try:
-            # Delete the workspace
-            workspace = frappe.get_doc("Workspace", workspace_name)
-            workspace.delete()
-            frappe.db.commit()
+            # Delete the workspace document
+            if frappe.db.exists("Workspace", workspace_name):
+                frappe.delete_doc("Workspace", workspace_name, force=1)
+                frappe.db.commit()
         except Exception as e:
             frappe.log_error(f"Error deleting workspace: {str(e)}", "Workspace Deletion Error")
 
