f049994cfb23c4126c2a4ad9370b493672412b2a
AlastairDare <dare.alastair@gmail.com>
2024-12-11 12:06:39 +0200

    f

--- transportation/transportation/doctype/doctype_label_config/doctype_label_config.js
+++ transportation/transportation/doctype/doctype_label_config/doctype_label_config.js
@@ -10,6 +10,7 @@ frappe.ui.form.on('DocType Label Config', {
             }
         `);
         
+        // Add Load Fields button
         frm.add_custom_button(__('Load Fields'), function() {
             load_doctype_fields(frm);
         });
@@ -28,11 +29,32 @@ frappe.ui.form.on('DocType Label Config', {
                 field.in_list_view = 1;
             }
         });
+
+        // Configure grid view settings
+        frm.set_query("doctype_name", function() {
+            return {
+                filters: {
+                    custom: 1  // Only show custom doctypes
+                }
+            };
+        });
     },
     
     doctype_name: function(frm) {
         if (frm.doc.doctype_name) {
             load_doctype_fields(frm);
+        }
+    },
+
+    before_save: function(frm) {
+        // Validate that custom labels are provided for active fields
+        let invalid_rows = frm.doc.field_labels.filter(row => 
+            row.is_active && !row.custom_label
+        );
+        
+        if (invalid_rows.length) {
+            frappe.throw(__('Please provide custom labels for all active fields or uncheck the Active checkbox'));
+            frappe.validated = false;
         }
     }
 });
@@ -67,3 +89,24 @@ function load_doctype_fields(frm) {
         }
     });
 }
+
+// Add handler for the child table
+frappe.ui.form.on('DocType Field Label', {
+    custom_label: function(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.custom_label && !row.is_active) {
+            row.is_active = 1;
+            frm.refresh_field('field_labels');
+        }
+    },
+    
+    is_active: function(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        if (row.is_active && !row.custom_label) {
+            frappe.show_alert({
+                message: __('Please provide a custom label for active fields'),
+                indicator: 'orange'
+            });
+        }
+    }
+});
