a1734bb1a4b26541283a722422f68de110893413
AlastairDare <dare.alastair@gmail.com>
2024-12-13 21:48:26 +0200

    f

--- transportation/transportation/doctype/trip/trip_list.js
+++ transportation/transportation/doctype/trip/trip_list.js
@@ -46,7 +46,7 @@ frappe.listview_settings['Trip'] = {
                 fieldname: 'from_date',
                 label: __('From Date'),
                 onchange: function() {
-                    validateAndSetDateFilter(listview);
+                    handleDateChange(listview);
                 }
             });
             $(fromDateField.wrapper).appendTo(dateFilterGroup);
@@ -58,7 +58,7 @@ frappe.listview_settings['Trip'] = {
                 fieldname: 'to_date',
                 label: __('To Date'),
                 onchange: function() {
-                    validateAndSetDateFilter(listview);
+                    handleDateChange(listview);
                 }
             });
             $(toDateField.wrapper).appendTo(dateFilterGroup);
@@ -83,15 +83,18 @@ frappe.listview_settings['Trip'] = {
     }
 };
 
-// Function to validate and set date filter
-function validateAndSetDateFilter(listview) {
+// Function to handle date changes
+function handleDateChange(listview) {
     const fromDate = listview.page.fields_dict.from_date.get_value();
     const toDate = listview.page.fields_dict.to_date.get_value();
 
     // Clear any existing error messages
     frappe.show_alert('', 0);
 
-    // Only validate if both dates are selected
+    // Remove existing date filters first
+    listview.filter_area.clear(true);
+
+    // Only proceed if both dates are selected
     if (fromDate && toDate) {
         if (fromDate > toDate) {
             frappe.show_alert({
@@ -102,16 +105,20 @@ function validateAndSetDateFilter(listview) {
             listview.page.fields_dict.to_date.set_value('');
             return;
         }
-        
-        // Set the date filter on the actual date field
-        listview.filter_area.add([[
-            'Trip',
-            'date',
-            'Between',
-            [fromDate, toDate]
-        ]]);
+
+        // Set the filters
+        listview.filter_area.add([
+            ["Trip", "docstatus", "<", "2"],
+            ["Trip", "date", ">=", fromDate],
+            ["Trip", "date", "<=", toDate]
+        ]);
     } else {
-        // Clear the date filter if either date is empty
-        listview.filter_area.remove('date');
+        // If dates are cleared, just add back the docstatus filter
+        listview.filter_area.add([
+            ["Trip", "docstatus", "<", "2"]
+        ]);
     }
+
+    // Refresh the list view
+    listview.refresh();
 }
