776891928a6a67e34febe5404937107ba7cae98a
AlastairDare <dare.alastair@gmail.com>
2024-12-13 22:15:28 +0200

    s

--- transportation/transportation/doctype/trip/trip_list.js
+++ transportation/transportation/doctype/trip/trip_list.js
@@ -6,31 +6,73 @@ frappe.listview_settings['Trip'] = {
     ],
 
     onload(listview) {
+        // Truck filter
         listview.page.add_field({
             fieldtype: 'Link',
             fieldname: 'truck',
             label: 'Truck',
-            options: 'Transportation Asset'
+            options: 'Transportation Asset',
+            onchange: () => {
+                const value = listview.page.fields_dict.truck.get_value();
+                if (value) {
+                    listview.filter_area.add([[listview.doctype, "truck", "=", value]]);
+                }
+                listview.refresh();
+            }
         });
 
+        // Invoice status filter
         listview.page.add_field({
             fieldtype: 'Select',
             fieldname: 'sales_invoice_status',
             label: 'Invoice Status',
-            options: '\nNot Invoiced\nInvoiced'
+            options: '\nNot Invoiced\nInvoiced',
+            onchange: () => {
+                const value = listview.page.fields_dict.sales_invoice_status.get_value();
+                if (value) {
+                    listview.filter_area.add([[listview.doctype, "sales_invoice_status", "=", value]]);
+                }
+                listview.refresh();
+            }
         });
 
+        // Billing customer filter
         listview.page.add_field({
             fieldtype: 'Link',
             fieldname: 'billing_customer',
             label: 'Billing Customer',
-            options: 'Customer'
+            options: 'Customer',
+            onchange: () => {
+                const value = listview.page.fields_dict.billing_customer.get_value();
+                if (value) {
+                    listview.filter_area.add([[listview.doctype, "billing_customer", "=", value]]);
+                }
+                listview.refresh();
+            }
         });
 
+        // Date range filter
         listview.page.add_field({
             fieldtype: 'DateRange',
-            fieldname: 'date',
-            label: 'Trip Date'
+            fieldname: 'date_range',
+            label: 'Trip Date',
+            onchange: () => {
+                const dates = listview.page.fields_dict.date_range.get_value();
+                if (dates && dates.length === 2) {
+                    listview.filter_area.add([[listview.doctype, "date", "between", dates]]);
+                }
+                listview.refresh();
+            }
+        });
+
+        // Clear filters button
+        listview.page.add_inner_button('Clear Filters', () => {
+            listview.filter_area.clear();
+            // Clear all field values
+            Object.values(listview.page.fields_dict).forEach(field => {
+                field.set_value('');
+            });
+            listview.refresh();
         });
     }
 };
