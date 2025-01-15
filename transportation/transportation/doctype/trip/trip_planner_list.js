4e33324ca10e342321771f25b3f1f7246f80f464
AlastairDare <dare.alastair@gmail.com>
2025-01-15 11:21:38 +0200

    trying different approachf or route

--- transportation/transportation/doctype/trip/trip_planner_list.js
+++ transportation/transportation/doctype/trip/trip_planner_list.js
@@ -1,28 +1,25 @@
-frappe.views.TripPlannerView = class TripPlannerView extends frappe.views.ListView {
-    setup_defaults() {
-        super.setup_defaults();
-        this.filters = [
-            ["Trip", "docstatus", "<", "2"],
-            ["Trip", "status", "=", "Planned"]
-        ];
-    }
+frappe.listview_settings['Planner'] = {
+    ...frappe.listview_settings['Trip'],
+    
+    filters: [
+        ["Trip", "docstatus", "<", "2"],
+        ["Trip", "status", "=", "Planned"]  // Default filter for Planned status
+    ],
 
-    setup_page() {
-        super.setup_page();
-        
+    onload(listview) {
         // Add Plan Trip button
-        this.page.add_button('Plan Trip', () => {
+        listview.page.add_button('Plan Trip', () => {
             frappe.new_doc('Trip');
         }, 'primary');
 
         // Add Truck filter
-        this.page.add_field({
+        listview.page.add_field({
             fieldtype: 'Link',
             fieldname: 'truck',
             label: 'Truck',
             options: 'Transportation Asset',
             onchange: () => {
-                const value = this.page.fields_dict.truck.get_value();
+                const value = listview.page.fields_dict.truck.get_value();
                 const filters = [
                     ["Trip", "docstatus", "<", "2"],
                     ["Trip", "status", "=", "Planned"]
@@ -30,19 +27,18 @@ frappe.views.TripPlannerView = class TripPlannerView extends frappe.views.ListVi
                 if (value) {
                     filters.push(["Trip", "truck", "=", value]);
                 }
-                this.filter_area.clear();
-                filters.forEach(filter => this.filter_area.add(filter));
+                refreshList(listview, filters);
             }
         });
 
         // Invoice status filter
-        this.page.add_field({
+        listview.page.add_field({
             fieldtype: 'Select',
             fieldname: 'sales_invoice_status',
             label: 'Invoice Status',
             options: '\nNot Invoiced\nInvoiced',
             onchange: () => {
-                const value = this.page.fields_dict.sales_invoice_status.get_value();
+                const value = listview.page.fields_dict.sales_invoice_status.get_value();
                 const filters = [
                     ["Trip", "docstatus", "<", "2"],
                     ["Trip", "status", "=", "Planned"]
@@ -50,19 +46,18 @@ frappe.views.TripPlannerView = class TripPlannerView extends frappe.views.ListVi
                 if (value) {
                     filters.push(["Trip", "sales_invoice_status", "=", value]);
                 }
-                this.filter_area.clear();
-                filters.forEach(filter => this.filter_area.add(filter));
+                refreshList(listview, filters);
             }
         });
 
         // Billing customer filter
-        this.page.add_field({
+        listview.page.add_field({
             fieldtype: 'Link',
             fieldname: 'billing_customer',
             label: 'Billing Customer',
             options: 'Customer',
             onchange: () => {
-                const value = this.page.fields_dict.billing_customer.get_value();
+                const value = listview.page.fields_dict.billing_customer.get_value();
                 const filters = [
                     ["Trip", "docstatus", "<", "2"],
                     ["Trip", "status", "=", "Planned"]
@@ -70,18 +65,17 @@ frappe.views.TripPlannerView = class TripPlannerView extends frappe.views.ListVi
                 if (value) {
                     filters.push(["Trip", "billing_customer", "=", value]);
                 }
-                this.filter_area.clear();
-                filters.forEach(filter => this.filter_area.add(filter));
+                refreshList(listview, filters);
             }
         });
 
         // Date range filter
-        this.page.add_field({
+        listview.page.add_field({
             fieldtype: 'DateRange',
             fieldname: 'date',
             label: 'Trip Date',
             onchange: () => {
-                const dateRange = this.page.fields_dict.date.get_value();
+                const dateRange = listview.page.fields_dict.date.get_value();
                 const filters = [
                     ["Trip", "docstatus", "<", "2"],
                     ["Trip", "status", "=", "Planned"]
@@ -90,22 +84,62 @@ frappe.views.TripPlannerView = class TripPlannerView extends frappe.views.ListVi
                 if (dateRange && dateRange.length === 2 && dateRange[0] && dateRange[1]) {
                     filters.push(["Trip", "date", "between", [dateRange[0], dateRange[1]]]);
                 }
-                this.filter_area.clear();
-                filters.forEach(filter => this.filter_area.add(filter));
+                refreshList(listview, filters);
             }
         });
 
         // Clear filters button
-        this.page.add_inner_button('Clear Filters', () => {
-            Object.values(this.page.fields_dict).forEach(field => {
+        listview.page.add_inner_button('Clear Filters', () => {
+            Object.values(listview.page.fields_dict).forEach(field => {
                 field.set_value('');
             });
-            this.filter_area.clear();
-            this.filter_area.add(["Trip", "docstatus", "<", "2"]);
-            this.filter_area.add(["Trip", "status", "=", "Planned"]);
+            refreshList(listview, [
+                ["Trip", "docstatus", "<", "2"],
+                ["Trip", "status", "=", "Planned"]
+            ]);
         });
     }
 };
 
-// Register the custom view
-frappe.views.view_registry['Trip Planner'] = 'TripPlannerView';
+function refreshList(listview, filters) {
+    frappe.call({
+        method: "frappe.client.get_list",
+        args: {
+            doctype: "Trip",
+            filters: filters,
+            fields: [
+                "name",
+                "owner",
+                "creation",
+                "modified",
+                "modified_by",
+                "_user_tags",
+                "_comments",
+                "_assign",
+                "_liked_by",
+                "docstatus",
+                "idx",
+                "status",
+                "sales_invoice_status",
+                "date",
+                "truck",
+                "delivery_note_number",
+                "billing_customer",
+                "rate",
+                "quantity"
+            ],
+            order_by: "modified desc"
+        },
+        callback: function(r) {
+            if (r.message) {
+                listview.data = r.message;
+                listview.render_list();
+            }
+        }
+    });
+}
+
+// Register the route
+frappe.views.ListView.route_to_planner = function() {
+    frappe.set_route('List', 'Trip', 'Planner');
+};
